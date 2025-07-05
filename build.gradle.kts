d7a637c4913421c804c2753c5068d4572452f174
Jockstrap6334 <Jockstrap6334@users.noreply.github.com>
2025-07-05 11:23:43 +0000

    Update build configuration to support testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,14 @@ plugins {
     kotlin("jvm") version "1.9.0"
 }
 
-dependencies {
-    testImplementation("junit:junit:4.13.2")
-    testImplementation(kotlin("test"))
-}
-
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.0")
 }
 
 tasks.test {
