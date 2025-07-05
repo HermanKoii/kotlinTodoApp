4903457c1906fbb4dc0cedfe1aa2b4a44ff30d0c
valin1993 <valin1993@users.noreply.github.com>
2025-07-05 10:15:00 +0000

    Mark duplicate Todo entity as deprecated

--- app/src/main/java/com/todoapp/data/entity/Todo.kt
+++ app/src/main/java/com/todoapp/data/entity/Todo.kt
@@ -1,2 +1,2 @@
 // Deprecated: Replaced by TodoItem in com.example.todoapp.data.model package
-// This file can be safely removed in future cleanup
+// This file is kept for historical reference and can be safely removed in future cleanup
