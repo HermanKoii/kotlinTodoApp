fcb8bf7e569e71abda413183e44858cdea788838
Aflame7121 <Aflame7121@users.noreply.github.com>
2025-07-05 10:13:51 +0000

    Update TodoItem model to match test specifications

--- app/src/main/java/com/todoapp/model/TodoItem.kt
+++ app/src/main/java/com/todoapp/model/TodoItem.kt
@@ -2,16 +2,8 @@ package com.todoapp.model
 
 import java.util.Date
 
-/**
- * Represents a single todo item in the application.
- *
- * @property id Unique identifier for the todo item
- * @property title Title or description of the todo item
- * @property dueDate Date when the todo item is due
- * @property isCompleted Indicates whether the todo item is completed
- */
 data class TodoItem(
-    val id: Long = 0,
+    val id: Long,
     val title: String,
     val dueDate: Date? = null,
     val isCompleted: Boolean = false
