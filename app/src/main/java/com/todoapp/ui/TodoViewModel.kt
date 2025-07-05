e419a286484fc4c79594795b0054f01001e385f4
region999 <region999@users.noreply.github.com>
2025-07-05 09:46:16 +0000

    Update TodoViewModel to use TodoItem instead of Todo

--- app/src/main/java/com/todoapp/ui/TodoViewModel.kt
+++ app/src/main/java/com/todoapp/ui/TodoViewModel.kt
@@ -3,10 +3,10 @@ package com.todoapp.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.todoapp.data.Todo
+import com.todoapp.model.TodoItem
 
 class TodoViewModel : ViewModel() {
     // Placeholder for todo items
-    private val _todoItems = MutableLiveData<List<Todo>>(emptyList())
-    val todoItems: LiveData<List<Todo>> = _todoItems
+    private val _todoItems = MutableLiveData<List<TodoItem>>(emptyList())
+    val todoItems: LiveData<List<TodoItem>> = _todoItems
 }
