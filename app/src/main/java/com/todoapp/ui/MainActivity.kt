265117f7ad33dc398c28ab1c5f85ce5670d4150a
region999 <region999@users.noreply.github.com>
2025-07-05 09:46:10 +0000

    Update MainActivity to use TodoListAdapter

--- app/src/main/java/com/todoapp/ui/MainActivity.kt
+++ app/src/main/java/com/todoapp/ui/MainActivity.kt
@@ -6,11 +6,12 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.todoapp.R
 import com.todoapp.databinding.ActivityMainBinding
+import com.todoapp.ui.adapter.TodoListAdapter
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var todoViewModel: TodoViewModel
-    private lateinit var todoAdapter: TodoAdapter
+    private lateinit var todoAdapter: TodoListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,14 +39,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupRecyclerView() {
-        todoAdapter = TodoAdapter(
+        todoAdapter = TodoListAdapter(
             onItemClick = { todo -> 
                 // TODO: Implement edit todo functionality 
             },
-            onDeleteClick = { todo ->
-                // TODO: Implement delete todo functionality
-            },
-            onCompleteToggle = { todo ->
+            onCompletionToggle = { todo ->
                 // TODO: Implement toggle complete functionality
             }
         )
